0ac2e91a1e42740220b270f973ac14fbd1ff4090
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-04-25 19:25:41 -0500

    build: creando repositorio para Articulo con los metodos: - encontrar Articulos por estado - encontrar Articulos por categoria - encontrar Articulos por propietario

---
+++ src/main/java/com/ingsoft/trueque/repository/ArticuloRepository.java
@@ -0,0 +1,20 @@
+package com.ingsoft.trueque.repository;
+
+import com.ingsoft.trueque.model.Articulo;
+import com.ingsoft.trueque.model.util.CategoriaArticulo;
+import com.ingsoft.trueque.model.util.EstadoArticulo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArticuloRepository extends JpaRepository<Articulo, Long> {
+
+    Page<Articulo> findAllByEstado(EstadoArticulo estado, Pageable pageable);
+    Page<Articulo> findAllByCategoria(CategoriaArticulo categoria, Pageable pageable);
+    Optional<Articulo> findByPropietarioNombre(String nombre);
+
+}
