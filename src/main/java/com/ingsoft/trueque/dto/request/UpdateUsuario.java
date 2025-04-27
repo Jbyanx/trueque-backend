524f73034bac0ba4b4e381dfec3771a1d1a8fdd3
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-04-27 16:58:43 -0500

    build: implementando crud de usuarios

---
+++ src/main/java/com/ingsoft/trueque/dto/request/UpdateUsuario.java
@@ -0,0 +1,16 @@
+package com.ingsoft.trueque.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+
+public record UpdateUsuario(
+        @NotBlank
+        String nombre,
+        @NotBlank
+        String apellido,
+        @Email
+        String correo
+) implements Serializable {
+}
