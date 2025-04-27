ab3fdf3e1b67c62f9d46c84b457d6d4aaa6d6122
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-04-26 23:45:41 -0500

    fix: organizando dependencias, agregando binding de mapstruct con lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,20 +26,35 @@ repositories {
 extra["springAiVersion"] = "1.0.0-M7"
 
 dependencies {
+	// Spring Boot
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// Base de datos
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
-	implementation("org.springframework.ai:spring-ai-markdown-document-reader")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("org.postgresql:postgresql")
-	developmentOnly("org.springframework.ai:spring-ai-spring-boot-docker-compose")
+
+	// MapStruct
+	implementation("org.mapstruct:mapstruct:1.6.3")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+
+	// Spring AI (si lo usas)
+	implementation("org.springframework.ai:spring-ai-markdown-document-reader")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	developmentOnly("org.springframework.ai:spring-ai-spring-boot-docker-compose")
+
+	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
 
 dependencyManagement {
 	imports {
@@ -49,4 +64,11 @@ dependencyManagement {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+	tasks.withType<JavaCompile> {
+		options.compilerArgs.add("-Amapstruct.defaultComponentModel=spring")
+		options.compilerArgs.add("-Amapstruct.unmappedTargetPolicy=IGNORE")
+	}
 }
