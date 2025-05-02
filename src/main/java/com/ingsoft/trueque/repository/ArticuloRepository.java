0b4007c262cece0102a5382e93a70ee473ce3993
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-02 18:57:10 -0500

    build: definiendo querys: - obtener articulos por id del propietario y estado - obtener articulos intercambiado por el usuario - obtener articulos por el estado - obtener los articulos por el id del propietario - obtener articulos por nombre del propietario

--- src/main/java/com/ingsoft/trueque/repository/ArticuloRepository.java
+++ src/main/java/com/ingsoft/trueque/repository/ArticuloRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,22 @@ public interface ArticuloRepository extends JpaRepository<Articulo, Long>, JpaSp
 
     Optional<Articulo> findByPropietarioNombre(String nombre);
 
+    Page<Articulo> getArticulosByPropietarioId(Long idUsuario, Pageable pageable);
+
+    Page<Articulo> getArticulosByEstado(EstadoArticulo estadoArticulo, Pageable pageable);
+
+    @Query("""
+    SELECT a
+        FROM Intercambio i  JOIN i.articuloUno a
+        WHERE i.estado = 'REALIZADO'
+        AND (i.usuarioUno.id = :usuarioId OR i.usuarioDos.id = :usuarioId)
+    UNION
+        SELECT a2   FROM Intercambio i2
+        JOIN i2.articuloDos a2
+            WHERE i2.estado = 'REALIZADO'
+    AND (i2.usuarioUno.id = :usuarioId OR i2.usuarioDos.id = :usuarioId)
+    """)
+    Page<Articulo> findArticulosIntercambiadosPorUsuario(Long usuarioId, Pageable pageable);
+
+    Page<Articulo> getArticulosByPropietarioIdAndEstado(Long idUsuario, Pageable pageable, EstadoArticulo estadoArticulo);
 }
