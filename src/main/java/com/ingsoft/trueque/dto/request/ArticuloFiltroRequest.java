5ecde18db21c286bc37e5c3e4d0193bb3364e3e5
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-02 19:00:49 -0500

    fix: imports innecesarios

--- src/main/java/com/ingsoft/trueque/dto/request/ArticuloFiltroRequest.java
+++ src/main/java/com/ingsoft/trueque/dto/request/ArticuloFiltroRequest.java
@@ -2,8 +2,6 @@ package com.ingsoft.trueque.dto.request;
 
 import org.springframework.data.domain.Pageable;
 
-import java.math.BigDecimal;
-
 public record ArticuloFiltroRequest(
         String categoria,
         String nombre,
