2ac1773e3072001c5e1ff82157282de4ef5aa8aa
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-02 20:47:35 -0500

    fix

--- src/main/resources/db/migration/V2__resenha-usuario-calificado.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE resenhas RENAME COLUMN id_autor TO id_usuario_calificante;
-
-ALTER TABLE resenhas ADD COLUMN id_usuario_calificado BIGINT;
-
-ALTER TABLE resenhas
-    ADD CONSTRAINT FK_resenha_usuario_calificado
-        FOREIGN KEY (id_usuario_calificado)
-            REFERENCES usuarios(id);
