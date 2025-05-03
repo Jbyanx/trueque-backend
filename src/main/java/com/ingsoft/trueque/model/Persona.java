b6b385a94af57cf41f49b75633e85f1005902409
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-02 19:08:15 -0500

    fix: eliminando validaciones de jakarta que no iban acordes con los requerimientos del sistema

--- src/main/java/com/ingsoft/trueque/model/Persona.java
+++ src/main/java/com/ingsoft/trueque/model/Persona.java
@@ -3,6 +3,7 @@ package com.ingsoft.trueque.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -19,14 +20,12 @@ public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
-    @Size(min = 5, max = 100, message = "El nombre debe tener de 5 a 100 caracteres")
+    @NotBlank(message = "el nombre no puede estar vacio")
     private String nombre;
-    @NotBlank
-    @Size(min = 5, max = 100, message = "El apellido debe tener de 5 a 100 caracteres")
+    @NotBlank(message = "el apellido no puede estar vacio")
     private String apellido;
     @Email
-    @Size(min = 5, max = 150, message = "El correo debe tener de 5 a 150 caracteres")
+    @NotNull(message = "el correo no puede estar vacio")
     private String correo;
     @NotBlank(message = "La contraseña no puede estar vacia")
     private String clave;
