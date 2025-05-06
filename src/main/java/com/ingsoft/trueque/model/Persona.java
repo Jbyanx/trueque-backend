a11ee22335dc5d20b04da31d0dc8ca0d7d7f5f7a
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-05 22:14:19 -0500

    feature(security): agregando rol y campo unique para correo

--- src/main/java/com/ingsoft/trueque/model/Persona.java
+++ src/main/java/com/ingsoft/trueque/model/Persona.java
@@ -1,5 +1,6 @@
 package com.ingsoft.trueque.model;
 
+import com.ingsoft.trueque.model.util.Rol;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -24,8 +25,12 @@ public class Persona {
     @NotBlank(message = "el apellido no puede estar vacio")
     private String apellido;
     @Email
+    @Column(nullable = false, unique = true)
     @NotNull(message = "el correo no puede estar vacio")
     private String correo;
     @NotBlank(message = "La contraseña no puede estar vacia")
     private String clave;
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Rol rol;
 }
