8d34a70a082ff44d4e69ffd891d8c0b54115c005
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-05 22:18:17 -0500

    feature(security): creando controlador para la autenticacion con los endpoints login y regisrar

--- src/main/java/com/ingsoft/trueque/controller/AuthController.java
+++ src/main/java/com/ingsoft/trueque/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.ingsoft.trueque.controller;
 
+import com.ingsoft.trueque.dto.request.LoginRequest;
 import com.ingsoft.trueque.dto.request.SaveUsuario;
-import com.ingsoft.trueque.dto.response.GetUsuario;
+import com.ingsoft.trueque.dto.response.GetUsuarioRegistrado;
+import com.ingsoft.trueque.dto.response.LoginResponse;
+import com.ingsoft.trueque.mapper.UsuarioMapper;
 import com.ingsoft.trueque.service.UsuarioService;
+import com.ingsoft.trueque.service.impl.security.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +20,23 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class AuthController {
     private final UsuarioService usuarioService;
+    private final UsuarioMapper usuarioMapper;
+    private final AuthService authService;
 
     @PostMapping("/registrar")
-    public ResponseEntity<GetUsuario> register(@ModelAttribute SaveUsuario saveUsuario) {
-        GetUsuario usuarioSaved = usuarioService.saveUsuario(saveUsuario);
+    public ResponseEntity<GetUsuarioRegistrado> registrarUsuario(@ModelAttribute SaveUsuario saveUsuario) {
+        GetUsuarioRegistrado usuarioSaved = usuarioMapper.toGetUsuarioRegistrado(usuarioService.saveUsuario(saveUsuario));
         URI createdUsuario = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(usuarioSaved.id())
                 .toUri();
         return ResponseEntity.created(createdUsuario).body(usuarioSaved);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(
+            @ModelAttribute @Valid LoginRequest authenticationRequest){
+
+        return ResponseEntity.ok(authService.login(authenticationRequest));
     }
 }
