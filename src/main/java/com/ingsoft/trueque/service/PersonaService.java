6b5305a6efa8b9dcb64b435d4a1857e8cef7f84a
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-05-07 09:55:39 -0500

    build: creando metodo para obtener personas por correo no key sensitive

--- src/main/java/com/ingsoft/trueque/service/PersonaService.java
+++ src/main/java/com/ingsoft/trueque/service/PersonaService.java
@@ -1,7 +1,7 @@
 package com.ingsoft.trueque.service;
 
-import com.ingsoft.trueque.dto.request.SavePersona;
 import com.ingsoft.trueque.dto.response.GetPersona;
+import com.ingsoft.trueque.model.Persona;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,4 +9,5 @@ public interface PersonaService {
     Page<GetPersona> getAllPersonas(Pageable pageable);
     GetPersona getPersonaById(Long id);
     void deletePersonaById(Long id);
+    Persona getPersonaByCorreo(String correo);
 }
